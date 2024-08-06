03cb9c0a31b6e78bf57d7c905ec2dc822267b26f
Lewis Freeman <lewis_3man@hotmail.com>
2024-08-06 13:27:29 +0000

    Deployment Commit

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_stroage',
+    'cloudinary_storage',
     'cloudinary',
     'to_do',
 ]
