4c05fc17cdf753416051e09fb097c176d03b1626
Lewis Freeman <lewis_3man@hotmail.com>
2024-08-07 23:32:22 +0000

    Update URL patterns to include authentication and to_do app

--- to_do_list/urls.py
+++ to_do_list/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('to_do.urls'), name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('to_do.urls')),
 ]
