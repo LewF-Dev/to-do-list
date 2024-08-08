a0553fe8cce08d58b73cfc7a75b36ac1a4741c75
Lewis Freeman <lewis_3man@hotmail.com>
2024-08-08 10:34:45 +0000

    Create views for listing tasks and adding new tasks

--- to_do/views.py
+++ to_do/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Task
+from .forms import TaskForm
+
 class HomePage(TemplateView):
     """
     Displays home page
     """
     template_name = 'index.html'
+
+@login_required
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST, request.FILES)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('index')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form})
