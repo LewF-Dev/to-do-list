b24262d104b80195086f4b0e70cae2cd2cde342f
Lewis Freeman <lewis_3man@hotmail.com>
2024-08-08 14:40:10 +0000

    Update layout and styling to better match wireframes

--- to_do/views.py
+++ to_do/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm  # Add this import
-from django.views.generic import TemplateView  # Add this import
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView
 from .models import Task
 from .forms import TaskForm
+from datetime import datetime
 
 class HomePage(TemplateView):
     template_name = 'index.html'
@@ -11,7 +12,13 @@ class HomePage(TemplateView):
 @login_required
 def index(request):
     tasks = Task.objects.filter(user=request.user)
-    return render(request, 'tasks/index.html', {'tasks': tasks})
+    current_date = datetime.now().strftime('%d / %m / %Y')
+    important_task = "Most important task description"  # You should replace this with actual logic
+    return render(request, 'tasks/index.html', {
+        'tasks': tasks,
+        'current_date': current_date,
+        'important_task': important_task
+    })
 
 @login_required
 def add_task(request):
