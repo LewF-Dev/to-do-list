58fc1cf680a19daecb515c14ed2cb1c6bd4dfecc
Lewis Freeman <lewis_3man@hotmail.com>
2024-08-19 14:04:48 +0000

    Fix issue where user can not select  whole hour times when setting time for a task

--- to_do/forms.py
+++ to_do/forms.py
@@ -4,11 +4,10 @@ from .models import Task, Profile
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        # Removed 'image' from fields
         fields = ['title', 'description', 'date', 'time']
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
-            'time': forms.TimeInput(attrs={'type': 'time'})
+            'time': forms.TimeInput(attrs={'type': 'time', 'step': 1})  # Added 'step' attribute
         }
 
 class ProfileForm(forms.ModelForm):
